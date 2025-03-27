16e488ba426795d51cbd3f66f240590e0d22c6e3
Ramiro2k2 <ramiro2k2@gmail.com>
2025-03-27 10:33:12 -0300

    tp1 resuelto

--- tp1_1.c
+++ tp1_1.c
@@ -5,6 +5,15 @@ int main(){
 
         printf("Hola Mundo");
 
+        int numero = 1;
+        int *p;
+        p=&numero;
+
+        printf("El contenido del puntero es : %d\n",*p);
+        printf("La direccion de memoria almaceda en el  puntero es : %p\n",p);
+        printf("La direccion de memoria almaceda en numero es : %p\n",&numero);
+        printf("La direccion de memoria del puntero es : %p\n",&p);
+        printf("El Tamanio de memoria de numero es : %zu\n",sizeof(numero));
 
 
 
